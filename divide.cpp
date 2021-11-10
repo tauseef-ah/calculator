0609eef636ed82955ceb75ced8d47e2f268048e9
tauseef-ah <tauseef.ahmed@intel.com>
2021-11-10 07:39:14 +0530

    BUGFIX enable new API to check number divisibility

--- divide.cpp
+++ divide.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 //using namespace std;
 
+int checkifnumdivisible(int a,int b)
+{
+  if(b==0) return 0;
+  else return 1;
+}
 int divide(int a,int b)
 {
   if(b == 1) return (a);
