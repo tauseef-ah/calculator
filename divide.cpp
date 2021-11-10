3680e234196b25c10dbd8f24a6f19c5ce2044512
tauseef-ah <tauseef.ahmed@intel.com>
2021-11-10 07:45:40 +0530

    BUGFIX enhancement of division

--- divide.cpp
+++ divide.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 //using namespace std;
 
+int checkifnumeratorprime(int a,int b)
+{
+  if(isprime(a)) return 1;
+  else return 0;
+}
 int checkifnumdivisible(int a,int b)
 {
-  if(b==0) return 0;
-  else return 1;
+  //if(b==0) return 0;
+  //else return 1;
+  return 0;
 }
 int divide(int a,int b)
 {
