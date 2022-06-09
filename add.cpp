007560c17b9ce188cdd2bfc7091ad266c4fedc60
Tauseef Ahmed <tauseef.ahmed@intel.com>
2022-06-09 15:10:24 +0530

    Updated malloc call

--- add.cpp
+++ add.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+
 using namespace std;
 
 int add(int a,int b)
 {
   int* num1 = new int(a);
   int* num2 = new int(b);
+  int* m3 = (int*) malloc(sizeof(int));
   
   return *num1+*num2;
 }
