cf0aa14f59e04e6c483332bedae96d0030242772
Tauseef Ahmed <tauseef.ahmed@intel.com>
2022-06-09 16:17:16 +0530

    Added alloca in loop

--- add.cpp
+++ add.cpp
@@ -8,5 +8,9 @@ int add(int a,int b)
   int* num2 = new int(b);
   int* m3 = (int*) malloc(sizeof(int));
   delete[] num1; 
+  for(int i=0;i<100;i++)
+  {
+      char* ptr = (char*)alloca(sizeof(int[9]));
+  }
   return *num1+*num2;
 }
